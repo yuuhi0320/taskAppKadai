8a6d1bebaa7d2d04e53696cf7f65bb0b1c18ed76
yuuhi0320 <tenpra0914@gmail.com>
2020-01-07 13:01:30 +0900

    pod installとgitignoreの作成まで実施

---
+++ taskApp/InputViewController.swift
@@ -0,0 +1,26 @@
+//
+//  InputViewController.swift
+//  taskApp
+//
+//  Created by 木村旭 on 2020/01/07.
+//  Copyright © 2020 asahi.kimura. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
--- taskApp/ViewController.swift
+++ taskApp/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.delegate = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        performSegue(withIdentifier: "cellSegue", sender: nil)
+        
+        return cell
+    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    //セルが削除可能なことを伝えるメソッド
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        
+        return . delete
+    }
+    
+    //deleteボタンが押されるときに呼ばれるメソッド
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
 }
+
 
